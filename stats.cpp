57c604bf6ca12fe41303f86399685e9a7515a0b8
Sirpitamilarasu <95046931+Sirpitamilarasu@users.noreply.github.com>
2022-05-06 08:45:16 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,21 +1,18 @@
-class Stats
-{
-public:
+#include "Stats.h"
+#include <algorithm>
+#include <vector>
 
-    double average;
-    double min;
-    double max;
-};
+
 
 Stats Statistics::ComputeStatistics(const std::vector<double>& vec) {
     //Implement statistics here
     Stats s;
 
     auto minmax = std::minmax_element(vec.begin(), vec.end());
-    s.min = minmax.first;
-    s.max = minmax.second;
+    s.min = *minmax.first;
+    s.max = *minmax.second;
 
-    double avg;
+    double avg = 0;
 
     for (auto& v : vec)
     {
@@ -23,6 +20,6 @@ Stats Statistics::ComputeStatistics(const std::vector<double>& vec) {
     }
 
     s.average = avg / vec.size();
-    
+
     return s;
 }
