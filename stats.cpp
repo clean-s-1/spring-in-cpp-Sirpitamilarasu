3ab78d2ef6c01436961f13762489c270efc5497c
Sirpitamilarasu <95046931+Sirpitamilarasu@users.noreply.github.com>
2022-05-06 10:02:30 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -23,3 +23,52 @@ Stats Statistics::ComputeStatistics(const std::vector<double>& vec) {
 
     return s;
 }
+
+
+EmailAlert::EmailAlert()
+{
+    emailSent = false;
+}
+
+EmailAlert::~EmailAlert()
+{
+}
+
+void EmailAlert::checkNRaiseAlert(const std::vector<double>& val)
+{
+    emailSent = true;
+}
+
+
+
+LEDAlert::LEDAlert()
+{
+    ledGlows = false;
+}
+
+LEDAlert::~LEDAlert()
+{
+}
+
+void LEDAlert::checkNRaiseAlert(const std::vector<double>& val)
+{
+    ledGlows = true;
+}
+
+
+
+StatsAlerter::StatsAlerter(float threshold, const std::vector<IAlerter*>& alerters)
+{
+    this->maxThresholdValue = threshold;
+    this->alertersList = alerters;
+}
+
+StatsAlerter::~StatsAlerter()
+{
+
+}
+
+void StatsAlerter::checkAndAlert(const std::vector<double>&)
+{
+
+}
